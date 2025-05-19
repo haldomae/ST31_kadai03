e0c8f00bde7eb8cfe1e839840a68d80858e7d9b4
haldomae <hal.domae@gmail.com>
2025-05-19 13:31:36 +0900

    ボタンにクリックリスナーを追加

--- app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai03/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai03/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.classnumber_00_domaekazuki.st31_kadai03
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,34 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // 画像の表示領域を取得
+        val imageArea: ImageView = findViewById(R.id.image_area)
+
+        // レイアウトからボタンを取得
+        val beforeButton: ImageButton = findViewById(R.id.before_button)
+        val nextButton: ImageButton = findViewById(R.id.next_button)
+
+        // 画像IDを配列として定義
+        val imageList = listOf(
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+        )
+
+        var currentIndex = 0
+
+        // 画像のクリックイベントを付与
+        beforeButton.setOnClickListener {
+            currentIndex = if (currentIndex - 1 < 0) imageList.size - 1 else currentIndex - 1
+            imageArea.setImageResource(imageList[currentIndex])
+        }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % imageList.size
+            imageArea.setImageResource(imageList[currentIndex])
         }
     }
 }
